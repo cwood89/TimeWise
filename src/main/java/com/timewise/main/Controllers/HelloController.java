7875eac4a77d810b493286c53bea7b6820642842
cwood89 <cwood89@mac.com>
2020-06-18 20:41:23 -0400

    changed import  in controllers

--- src/main/java/com/timewise/main/Controllers/HelloController.java
+++ src/main/java/com/timewise/main/Controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controllers;
+package com.timewise.main.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
