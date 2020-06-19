e18ccafb8d43e150775a08553c6a801ef073e51a
cwood89 <cwood89@mac.com>
2020-06-18 21:37:25 -0400

    implmented repository in main class

--- src/main/java/com/timewise/main/Controllers/NewsController.java
+++ src/main/java/com/timewise/main/Controllers/NewsController.java
@@ -4,22 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 import com.timewise.main.Domain.Customer;
 import com.timewise.main.Repository.CustomerRepository;
 
 @RestController
 public class NewsController {
-  private final CustomerRepository repo;
+  private CustomerRepository repo;
 
   @GetMapping("/news")
   Iterable<Customer> newsHello() {
-    return repo.findAll();
+    Iterable<Customer> customer = repo.findAll();
+    return customer;
   }
 
   @PostMapping("/news")
   public String postMethod() {
+
     return "String";
   }
 
